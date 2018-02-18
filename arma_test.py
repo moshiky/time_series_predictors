49d163dcefcea1d706da4488b630c167800afc33
moshiky_laptop <moshec40@gmail.com>
2018-02-18 16:12:27 +0200

    * genelarize metrics logging * add lag support in sigmoid

--- arma_test.py
+++ arma_test.py
@@ -6,10 +6,12 @@ from logger import Logger
 
 
 DATASET_FILE_PATH = r'datasets/author_h_index.csv'
-LAG_SIZE = 0
+LAG_SIZE = 6
 INITIAL_HISTORY_SIZE = 10
 NUMBER_OF_PREDICTIONS_AHEAD = 15
 LOGGING_INTERVAL = 100
+SHOULD_PLOT = False
+IS_ONLINE = True
 
 
 def predict_using_online_mode(
@@ -43,7 +45,7 @@ def predict_using_online_mode(
                     with_c,
                     update_model=update_model,
                     use_sample_data=use_sample_data,
-                    should_plot=False,
+                    should_plot=SHOULD_PLOT,
                     record_id=record_index
                 )
 
@@ -56,13 +58,7 @@ def predict_using_online_mode(
                 model_error_metrics[metric_name] = list()
             model_error_metrics[metric_name].append(error_metrics[metric_name])
 
-    # return avg metrics
-    avg_metrics = dict()
-    for metric_name in model_error_metrics.keys():
-        if len(model_error_metrics[metric_name]) > 0:
-            avg_metrics[metric_name] = sum(model_error_metrics[metric_name]) / len(model_error_metrics[metric_name])
-
-    return avg_metrics
+    return model_error_metrics
 
 
 def run_ar(logger):
@@ -85,20 +81,23 @@ def run_arma(logger, order=None):
         initial_history_size=INITIAL_HISTORY_SIZE,
         number_of_predictions_ahead=NUMBER_OF_PREDICTIONS_AHEAD,
         lag_size=LAG_SIZE,
-        update_model=True,
-        use_sample_data=True
+        update_model=IS_ONLINE,
+        use_sample_data=IS_ONLINE
     )
 
 
 def main(logger):
-    ar_values = run_ar(logger)
-    ma_values = run_ma(logger)
-    arma_values = run_arma(logger)
+    ar_metrics = run_ar(logger)
+    # ma_values = run_ma(logger)
+    # arma_values = run_arma(logger)
 
     # print values
-    logger.log('AR avg. performance: {ar_values}'.format(ar_values=ar_values))
-    logger.log('MA avg. performance: {ma_values}'.format(ma_values=ma_values))
-    logger.log('ARMA avg. performance: {arma_values}'.format(arma_values=arma_values))
+    logger.log('-- avg. performance:')
+    utils.log_metrics_dict(logger, ar_metrics)
+
+    # logger.log('AR avg. performance: {ar_values}'.format(ar_values=ar_values))
+    # logger.log('MA avg. performance: {ma_values}'.format(ma_values=ma_values))
+    # logger.log('ARMA avg. performance: {arma_values}'.format(arma_values=arma_values))
 
 
 def calc_rate(logger):
@@ -109,7 +108,7 @@ def calc_rate(logger):
     logger.log('done.')
 
 
-def sigmoid_test(logger, is_online, should_plot):
+def sigmoid_test(logger, is_online, should_plot, lag_size):
     logger.log('load records..')
     data_records = utils.parse_csv(DATASET_FILE_PATH, smoothing_level=1, should_shuffle=False)
 
@@ -135,18 +134,19 @@ def sigmoid_test(logger, is_online, should_plot):
             if is_online:
                 mid_max_rate = SigmoidCurve.MID_MAX_RATE
                 for i in range(NUMBER_OF_PREDICTIONS_AHEAD):
+                    tmp_history = train_set + test_set[:i]
                     next_prediction = \
                         SigmoidCurve.fit_and_predict(
-                            train_set + test_set[:i], 1, mid_max_rate=mid_max_rate
+                            tmp_history[-lag_size:], 1, mid_max_rate=mid_max_rate
                         )[0]
                     sigmoid_predictions.append(next_prediction)
-                    mid_max_rate *= 0.9
-                    mid_max_rate = max(1.2, mid_max_rate)
+                    mid_max_rate *= 0.85
+                    mid_max_rate = max(1.7, mid_max_rate)
 
             else:
                 sigmoid_predictions = \
                     SigmoidCurve.fit_and_predict(
-                        train_set, NUMBER_OF_PREDICTIONS_AHEAD
+                        train_set[-lag_size:], NUMBER_OF_PREDICTIONS_AHEAD
                     )
 
             # plot predictions
@@ -154,7 +154,7 @@ def sigmoid_test(logger, is_online, should_plot):
                 utils.plot_graph_and_prediction(
                     train_set + test_set,
                     sigmoid_predictions,
-                    INITIAL_HISTORY_SIZE,
+                    INITIAL_HISTORY_SIZE+1,
                     'sigmoid__{record_id}'.format(record_id=record_id)
                 )
 
@@ -164,16 +164,14 @@ def sigmoid_test(logger, is_online, should_plot):
                     model_error_metrics[metric_name] = list()
                 model_error_metrics[metric_name].append(error_metrics[metric_name])
 
+    # log metrics
     logger.log('-- avg. performance:')
-    for metric_name in model_error_metrics.keys():
-        if len(model_error_metrics[metric_name]) > 0:
-            avg_metric = sum(model_error_metrics[metric_name]) / len(model_error_metrics[metric_name])
-            logger.log('{key} : {avg_value}'.format(key=metric_name, avg_value=avg_metric))
+    utils.log_metrics_dict(logger, model_error_metrics)
 
 
 if __name__ == '__main__':
     main_logger = Logger()
-    # main(main_logger)
+    main(main_logger)
     # calc_rate(main_logger)
-    sigmoid_test(main_logger, is_online=True, should_plot=False)
+    # sigmoid_test(main_logger, is_online=IS_ONLINE, should_plot=SHOULD_PLOT, lag_size=LAG_SIZE)
 
