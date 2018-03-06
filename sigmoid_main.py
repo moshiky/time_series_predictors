4966601e8542209570dc0b575e5ff60f5f8fefc3
moshiky_laptop <moshec40@gmail.com>
2018-03-06 12:51:19 +0200

    * add h-index mean values graph

--- sigmoid_main.py
+++ sigmoid_main.py
@@ -5,6 +5,18 @@ from logger import Logger
 import utils
 
 
+def get_mean_h_index_graph():
+    y_for_x = {
+        1: 1.038427771, 2: 1.228371716, 3: 1.522827041, 4: 1.848247451, 5: 2.196933883,
+        6: 2.560174242, 7:2.948740461, 8: 3.357871277, 9: 3.789862903, 10: 4.245053015,
+        11: 4.714898359, 12: 5.210474775, 13: 5.71287229, 14: 6.24127102, 15: 6.765482542,
+        16: 7.3025596, 17: 7.851995678, 18: 8.402275951, 19: 8.945026001, 20: 9.44381036,
+        21: 9.890896198, 22: 10.24885209, 23: 10.59303554, 24: 10.93647228, 25: 11.23032671,
+        26: 11.5080625, 27: 11.79206063, 28: 12.04333498, 29: 12.26126475, 30: 12.41589939
+    }
+    return y_for_x
+
+
 def generate_series(w_vector):
     # define settings
     l_param, a_param, c_param = w_vector
@@ -87,8 +99,8 @@ def main():
 
     # load series
     logger.log('# load series..')
-    original_w_vector = np.array([70, -0.33, 70], dtype=np.float64)
-    initial_w_vector = np.array([60, -1, 80], dtype=np.float64)
+    original_w_vector = np.array([15, -0.17, 11], dtype=np.float64)
+    initial_w_vector = np.array([10, -1, 14], dtype=np.float64)
     train, test = generate_series(original_w_vector)
 
     # create fitter
@@ -98,8 +110,9 @@ def main():
     logger.log('# fit sigmoid params..')
     fitted_w_vector = \
         gd_fitter.fit_and_predict_gd_online(
-            train, len(original_w_vector), is_stochastic=True, fit_limit_rank=5e-2, plot_progress=True, gamma_0=1e-7,
-            first_w=initial_w_vector, gamma_change_mode=GradientDescentFitter.GAMMA_INCREASING
+            train, len(original_w_vector), is_stochastic=True, fit_limit_rank=5e-2, plot_progress=True, gamma_0=1e-6,
+            first_w=initial_w_vector, gamma_change_mode=GradientDescentFitter.GAMMA_INCREASING,
+            evaluation_plot_mode=GradientDescentFitter.PLOT_RECENT
         )
 
     logger.log('original w vector: {original_vector}'.format(original_vector=original_w_vector))
